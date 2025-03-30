cf72aedf7d8a74d4e12f4078a6d6cb321e0ce040
JoroDesu <russelguiwan@gmail.com>
2025-03-30 15:41:23 +0800

    about v3 - first part done

--- src/pages/landing/About.jsx
+++ src/pages/landing/About.jsx
@@ -13,16 +13,34 @@ const About = () => {
       </div>
     </div>
 
-      <div className='w-screen h-auto min-h-[79rem] bg-black'>
-       <div className='max-[140rem] xl:max-w-[140rem] 3xl:max-w-[180rem] h-auto min-h-[79rem] mx-auto bg-amber-800'>
-          <div className='flex flex-col sm:flex-row'>
-            <span className='w-full sm:w-1/2  h-full bg-amber-200'>left message</span>
-            <span className='w-full sm:w-1/2 h-full bg-amber-600'>right message</span>
+      <div className='w-screen h-auto min-h-[79rem]'>
+       <div className='max-w-[140rem]  3xl:max-w-[180rem] h-auto min-h-[79rem] mx-auto text-[3rem]'>
+          <div className='flex flex-col sm:flex-row mt-[10rem]'>
+            <span className='w-full sm:w-2/5  h-full m-[2rem]'>
+              <p className=" max-w-full h-auto items-end text-justify">
+              The Museo Bulawan, also known as the Community Museum of Camarines Norte, embodies the rich cultural and historical essence of the province. Its origins trace back to 1995, when the construction of a new neo-classical provincial capitol building provided a fitting backdrop for the establishment of a museum. The Greco-Roman architecture of the building inspired a vision of timeless grandeur, making it an ideal setting to house artifacts and exhibits that celebrate the province's heritage.
+                <img src="../src/assets/Bulawan-Museum-14 1.png" alt="About Us" className="mt-6 w-full h-auto rounded-lg shadow-lg" />
+              </p>
+            </span>
+            <span className='w-full sm:w-3/5 h-full m-[2rem]'>
+                <h1 className='text-[4rem] font-bold'>Lorem ipsum dolor sit amet. Sit maxime delectus et amet itaque rem esse perferendis et ratione accusantium non natus rerum non similique pariatur. 
+                  </h1>
+                  <br />
+                  <br />
+                  <p className='font-bold'>Humble Beginnings
+                  </p>
+                    <br />
+                   
+                        <p>Initially, the museum was situated in a modest space at the center of the capitol building's frontage, directly facing the main entrance. Despite its limited size, this initial venue served as a hub for various periodic exhibits and activities. It became an effective platform for the provincial government to foster cultural awareness and education among the youth and the community at large.
+                        <br />
+                                            <br />
+                        Through collaborations with national organizations such as the National Commission for Culture and the Arts (NCCA), the National Museum, and the National Historical Institute, the museum hosted numerous events that sparked a growing interest in local history and cultural preservation. These partnerships deepened the community's sense of identity and strengthened their connection to the national narrative.
+                        <br />
+                                            <br />
+                        This initial phase of the museum also served as a testament to the community's commitment to preserving their heritage despite spatial limitations. The intimate setting encouraged meaningful interactions between visitors and exhibits, fostering a deeper appreciation for the artifacts on display. Local artists, historians, and cultural advocates found a welcoming space to share their work and knowledge, further enriching the museum's offerings. These early efforts laid the groundwork for a vibrant cultural hub that would eventually grow into a larger and more comprehensive institution, embodying the community's dedication to celebrating and safeguarding their shared history.</p>
+                 </span>
           </div>
-          <div className='flex flex-col sm:flex-row'>
-            <span className='w-full sm:w-1/2  h-full bg-amber-200'>left message</span>
-            <span className='w-full sm:w-1/2 h-full bg-amber-600'>right message</span>
-          </div>
+         
           
 
 
