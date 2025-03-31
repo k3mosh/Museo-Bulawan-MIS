9fc0642e823a86ee2a2a524235189ec989cc13a5
k3mosh <cocsmurftzy@gmail.com>
2025-03-31 18:52:31 +0800

    admin height fixed to screen

--- src/pages/admin/Artifact.jsx
+++ src/pages/admin/Artifact.jsx
@@ -4,7 +4,7 @@ import AdminNav from '../../components/navbar/AdminNav'
 const Artifact = () => {
   return (
     <>
-      <div className='w-screen min-h-[79.8rem] h-auto bg-white flex pt-[7rem]'>
+      <div className='w-screen min-h-[79.8rem] h-screen bg-white flex pt-[7rem]'>
         <div className='bg-[#1C1B19] w-auto min-h-full h-auto'>
           <AdminNav />
         </div>
--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -8,7 +8,7 @@ const Dashboard = () => {
 
   return (
     <>
-      <div className='w-screen min-h-[79.8rem] h-auto bg-white flex pt-[7rem]'>
+      <div className='w-screen min-h-[79.8rem] h-screen bg-white flex pt-[7rem]'>
         <div className='bg-[#1C1B19] w-auto min-h-full h-auto'>
           <AdminNav />
         </div>
