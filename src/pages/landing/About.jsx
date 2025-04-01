45bd846d97c893e099c72423506796096dce09ef
JoroDesu <russelguiwan@gmail.com>
2025-04-01 20:08:30 +0800

    about v6 - ung carousel sa last part nalang

--- src/pages/landing/About.jsx
+++ src/pages/landing/About.jsx
@@ -15,19 +15,19 @@ const About = () => {
       </div>
     </div>
 
-      <div className='w-screen h-auto min-h-[79rem] '>
+      <div className='w-screen h-auto min-h-[79rem] mx-uto font-hina'>
        <div className='max-w-[140rem]  3xl:max-w-[180rem] h-auto min-h-[79rem] mx-auto text-[3rem]'>
-          <div className='flex flex-col sm:flex-row mt-[10rem]'>
+          <div className='flex flex-col sm:flex-row  mt-[10rem]'>
             
-            <span className='w-full sm:w-2/5  h-full m-[2rem]'>
+            <span className='w-full sm:w-2/5  h-full m-auto sm:m-[2rem]'>
             <div className=" max-w-full h-auto items-end text-justify">
-             <p>              The Museo Bulawan, also known as the Community Museum of Camarines Norte, embodies the rich cultural and historical essence of the province. Its origins trace back to 1995, when the construction of a new neo-classical provincial capitol building provided a fitting backdrop for the establishment of a museum. The Greco-Roman architecture of the building inspired a vision of timeless grandeur, making it an ideal setting to house artifacts and exhibits that celebrate the province's heritage.
+             <p>The Museo Bulawan, also known as the Community Museum of Camarines Norte, embodies the rich cultural and historical essence of the province. Its origins trace back to 1995, when the construction of a new neo-classical provincial capitol building provided a fitting backdrop for the establishment of a museum. The Greco-Roman architecture of the building inspired a vision of timeless grandeur, making it an ideal setting to house artifacts and exhibits that celebrate the province's heritage.
                 <img src="../src/assets/Bulawan-Museum-14 1.png" alt="About Us" className="mt-6 w-full h-auto rounded-lg shadow-lg" />
               </p>
               </div>
               
             </span>
-            <span className='w-full sm:w-3/5 h-full m-[2rem] text-justify'>
+            <span className='w-full sm:w-3/5 h-full m-auto sm:m-[2rem] text-justify'>
                 <h1 className='text-[4rem] font-bold'>"A museum is a bridge that connects the past to the present, inspiring the future."
                   </h1>
                   <br />
@@ -46,14 +46,14 @@ const About = () => {
         <div>
         <div className='flex flex-col sm:flex-row mt-[10rem] text-justify'>
             {/* 2nd Part */}
-          <span className='w-full sm:w-1/2  h-full m-[2rem] '>
+          <span className='w-full sm:w-1/2  h-full m-auto sm:m-[2rem] '>
           <div>
               <h1 className='text-[4rem] font-bold mb-[6rem]'>“Heritage is not what we inherit from the past; it is what we preserve for the future.”</h1>
               <img src="../src/assets/455363415_812761527719886_1195461782753847821_n.png" alt="About Us" className="mt-6 w-full h-auto rounded-lg shadow-lg" />
 
            </div> 
            </span>
-           <span className='w-full sm:w-1/2 h-full m-[2rem] mt-[13.5rem] '>
+           <span className='w-full sm:w-1/2 h-full m-auto sm:m-[2rem] mt-[13.5rem] '>
               <div>
                 <h1 className='font-bold'>Expansion and Transformation</h1>
                 <br />
@@ -72,7 +72,7 @@ const About = () => {
         <div>
         <div className='flex flex-col sm:flex-row mt-[3rem] text-justify'>
            
-          <span className='w-full sm:w-2/5  h-full m-[2rem] '>
+          <span className='w-full sm:w-2/5  h-full m-auto sm:m-[2rem] '>
           <div>
             <h1 className='text-[5rem] font-bold'>
               Museo Bulawan: A Beacon of Cultural Wealth
@@ -107,7 +107,7 @@ const About = () => {
             In addition to these exhibits, the museum hosts periodic events and activities designed to engage children and adults alike, further solidifying its role as a cornerstone of cultural education and community engagement.
            </h1>
             </span>
-            <span className='w-full sm:w-2/5 h-full m-[2rem] flex justify-center items-center'>
+            <span className='w-full sm:w-2/5 h-full m-auto sm:m-[2rem] flex justify-center items-center'>
               <div className='ml-[2rem]'>
                 <h1 className='text-[4.3rem] font-bold'>
                   A Living Heritage
@@ -161,7 +161,7 @@ const About = () => {
                   <h1 className="text-[4rem] mb-[3rem] text-[#FCE6BC]">A Message From The Museum Curator's</h1>
                     <h1 className="text-[5rem] font-bold mb-[3rem] text-[#FCE6BC]">Mr. Abel C. Icatlo</h1>
                       <img src="../src/assets/467396235_122120510066524881_2018490976163991958_n 2.png" alt="Abel C. Icatlo" className='w-[30rem] h-auto mx-auto'/>
-                      <p className="w-auto h-full text-[2.4rem] text-center leading-[4rem] text-white mx-auto">
+                      <p className="w-auto sm:w-[130rem] h-full text-[2.4rem] text-center leading-[4rem] text-white mx-auto mt-[5rem] mb-[10rem]">
                             Through the pages of history we saw the searing revolutionary fire of the nationalist heroes of Camarines Norte who convulsed in a determined uprising in Daet in April 1898 and later established the First Rizal Monument in December of that year. The seeds of freedom sown by the local heroes of this Province engendered an awakened nation and a people who were wont to pay the price of freedom. Through the wars and dictatorship, the people of Camarines Norte seasoned the challenges with perseverance and fortitude.
                             Museo Bulawan, which is a community museum, attempts to capture the rich panorama of struggle of the Local people. History aside, Museo Bulawan, showcases as well the exquisite treasure of this province which is gold. Actually, bulawan is a Filipino word for gold and it is said that Camarines Norte is a province that sits in a mound of gold.
                             Not only does the Provincial Government of Camarines Norte mind itself on the material development of the people but more so, it strives to zero in on the software of social and economic development which is the cultural, ethical and moral upliftment of the people. And there is no other way by which this can be effectively done except through the powerful tool of culture and arts.
@@ -175,7 +175,33 @@ const About = () => {
             {/* 7th part */}
             <div>
               <img src="../src/assets/8900f8_517f4f9ec7054b88a23b301bf31d6596~mv2 1.png" alt="" />
+                <div className='text-center mt-[3rem] '>
+                  <h1 className='text-[5rem] mb-[3rem] font-bold'>
+                    Key People
+                  </h1>
+                  <h2 className='font-bold'>
+                    Pure Professionalism
+                  </h2>
+                  <p className='w-auto sm:w-[130rem] mx-auto m-[2rem]'>
+                    Our dedicated team of experienced professionals are always on the ball, utilizing their unique skills and passion to move the work of Museum. We’re always pushing ourselves to stay ahead of the curve and striving to perfect our programs. Meet some of our incredible employees below.
+                  </p>
+                </div>
             </div>
+
+            {/* 8th part */}
+            <div>
+              <div>
+                <div className="w-full h-auto  flex items-center justify-center text-center px-8 bg-[#1C1B19] ">
+                  <div className='w-[140rem] mt-[10rem]'>
+                    <img src="../src/assets/467396235_122120510066524881_2018490976163991958_n 2.png" alt="Abel C. Icatlo" className='w-[30rem] h-auto mx-auto'/>
+                        <h1 className="text-[5rem] font-bold mt-[2rem] mb-[2rem] text-[#FCE6BC]">Abel C. Icatlo</h1>
+                          <h1 className="text-[3rem] mb-[3rem] text-[#FCE6BC]">Museum Curator</h1>
+                            
+                  </div>
+                </div>
+              </div>
+            </div>
+
       </div>
     </div>
     </>
