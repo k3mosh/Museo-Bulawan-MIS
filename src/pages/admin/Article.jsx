11297ac717d567c32f87dac0021879be7e38add6
k3mosh <cocsmurftzy@gmail.com>
2025-04-05 14:50:37 +0800

    article init

--- src/pages/admin/Article.jsx
+++ src/pages/admin/Article.jsx
@@ -4,9 +4,237 @@ import AdminNav from '../../components/navbar/AdminNav'
 const Article = () => {
   return (
     <>
-      <div className='w-screen min-h-[79.8rem] h-screen bg-white flex pt-[7rem]'>
-        <div className='bg-[#1C1B19] w-auto min-h-full h-auto'>
+      <div className='w-screen min-h-[79.8rem] h-screen bg-[#F0F0F0] select-none flex pt-[7rem]'>
+        <div className='bg-[#1C1B19] w-auto min-h-full h-full min-w-[6rem] sm:min-w-auto'>
           <AdminNav />
+        </div>
+        <div className='w-full min-h-full h-full flex flex-col gap-y-10 px-7 pb-7 pt-[4rem] overflow-scroll'>
+          <span className=' text-5xl font-semibold'>Articles</span>
+          <div className='w-full h-full flex flex-col xl:flex-row gap-y-5 xl:gap-y-0 xl:gap-x-5 '>
+            <div className='min-w-[34rem] h-full flex flex-col gap-y-7'>
+              {/* info bar */}
+              <div className='w-full max-w-[35rem] text-gray-500 min-h-[5rem] flex justify-start py-2 gap-x-2'>
+                <button className='px-4 h-full border-1 border-gray-500 rounded-lg cursor-pointer'>
+                  <span className='text-2xl font-semibold'>Artifact</span>
+                </button>
+                
+              </div>
+
+              <div className='w-full h-full flex flex-col gap-y-[5rem]'>
+                <div className='bg-[#161616] px-4 h-[5rem] flex justify-between items-center rounded-sm'>
+                  <span className='text-2xl text-white font-semibold'>Articles</span>
+                  <div className='w-[6rem] h-[3rem] bg-[#D4DBFF] flex items-center justify-center rounded-md'>
+                    <span className='text-2xl text-black font-semibold'>255</span>
+                  </div>
+                </div>
+
+                <div className='w-full h-auto flex flex-col gap-y-7'>
+                  {/* Date */}
+                  <span className='text-2xl font-semibold text-[#727272]'>January 8, 2025</span>
+                  <div className='w-full h-fit flex justify-between items-center'>
+                    <span className='text-2xl font-semibold '>Posted</span>
+                    <div className='w-[5rem] h-[2rem] flex items-center bg-[#D4DBFF] rounded-md justify-center'>
+                      <span className='text-2xl font-semibold'>545</span>
+                    </div>
+                  </div>
+
+                  <div className='w-full h-fit flex justify-between items-center'>
+                    <span className='text-2xl font-semibold '>Pending Edit</span>
+                    <div className='w-[5rem] h-[2rem] flex items-center bg-[#D4DBFF] rounded-md justify-center'>
+                      <span className='text-2xl font-semibold'>10</span>
+                    </div>
+                  </div>
+
+                  <button class="cursor-pointer flex items-center justify-between w-full px-6 py-4 bg-[#6BFFD5] text-black font-medium">
+                    <span className='text-2xl font-semibold'>Add New Article</span>
+                    <span class="border-2 border-black rounded-full p-2 flex items-center justify-center">
+                      <i class="fas fa-plus text-xl "></i>
+                    </span>
+                  </button>
+
+                </div>
+                
+              </div>
+
+              
+
+            </div>
+            
+            <div className=' w-full h-full flex flex-col gap-y-7 overflow-x-scroll overflow-y-scroll'>
+              {/* table */}
+              <div className='min-w-[94rem] min-h-[5rem] py-2 flex items-center gap-x-2'>
+                {/* toolbar */}
+                <button className='px-3 h-full rounded-lg border-1 border-gray-500  cursor-pointer'>
+                  <i class="text-gray-500 fa-regular fa-calendar text-4xl"></i>
+                </button>
+
+                
+         
+
+                <div class="relative h-full min-w-[20rem]">
+                  <i className="text-2xl fa-solid fa-magnifying-glass absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer"></i>
+                  <input type="text" placeholder="Search History" class="h-full pl-10 pr-3 py-2  border-1 border-gray-500 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500"></input>
+                </div>
+
+
+                <div class="relative h-full  min-w-48">
+                  <input type="text" placeholder="Filter..." class="pl-4 h-full text-2xl pr-8 py-2 border-1 border-gray-500  rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500"></input>
+                  <i class="cursor-pointer text-2xl fas fa-plus absolute right-3 top-1/2 -translate-y-1/2 text-gray-500"></i>
+                </div>
+
+                <div class="relative h-full min-w-48">
+                  <select class="appearance-none border-1 border-gray-500  h-full text-2xl rounded-lg text-gray-500 w-full py-2 pl-4 pr-8 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                    <option>All Actions</option>
+                    <option>Action 1</option>
+                    <option>Action 2</option>
+                  </select>
+                  <i class="text-2xl fas fa-caret-down absolute right-3 top-1/2 -translate-y-1/2 text-gray-600"></i>
+                </div>
+              </div>
+              
+              <div className='min-w-[94rem] w-full font-semibold h-fit grid grid-cols-6 justify-between'>
+              {/* table headers */}
+                <div className='text-[#727272] text-2xl border-l-1 px-3 py-2 cols-span-1'>
+                  <span>Date</span>
+                </div>
+                <div className='text-[#727272] text-2xl border-l-1 px-3 py-2  cols-span-1'>
+                <span>Visitor Name</span>
+
+                </div>
+                <div className='text-[#727272] text-2xl border-l-1 px-3 py-2  cols-span-1'>
+                <span>Title</span>
+
+                </div>
+                <div className='text-[#727272] text-2xl border-l-1 px-3 py-2  cols-span-1'>
+                <span>Status</span>
+
+                </div>
+                <div className='text-[#727272] text-2xl border-l-1 px-3 py-2  cols-span-1'>
+                <span>Transfer Status</span>
+
+                </div>
+
+                <div className='text-[#727272] justify-between flex text-2xl border-l-1 pl-3  cols-span-1'>
+                <span className='my-2'>Updated</span>
+                <div className='text-[#727272] text-2xl border-l-1 px-3 py-2 w-[7rem] cols-span-1'>
+                  <span>Action</span>
+                </div>
+
+                </div>
+                
+              </div>
+              <div className='w-full min-w-[94rem] h-auto flec flex-col'>
+                {/* table data */}
+
+                
+                <div className='min-w-[94rem] text-xl h-fit font-semibold grid grid-cols-6 justify-between cursor-pointer hover:bg-gray-300'>
+                  <div className='  px-4 pt-1 pb-3  border-t-1 border-t-gray-400 cols-span-1'>
+                    <span>02-19-2024</span>
+                  </div>
+                  <div className='  px-4 pt-1 pb-3  border-t-1 border-t-gray-400 cols-span-1'>
+                    <span>Olivia Harper</span>
+
+                  </div>
+                  <div className='  px-4 pt-1 pb-3  border-t-1 border-t-gray-400 cols-span-1'>
+                    <span>Perlas ng silanganan</span>
+
+                  </div>
+                  <div className='  px-4 py-4  border-t-1 border-t-gray-400 cols-span-1'>
+                    <span className='text-white bg-[#9C7744] border-1 border-black rounded-md px-15 py-1'>Accepted</span>
+
+                  </div>
+                  <div className='  px-4 py-4  border-t-1 border-t-gray-400 cols-span-1'>
+                    <span className='text-white bg-[#9C7744] border-1 border-black rounded-md px-15 py-1'>Accepted</span>
+
+                  </div>
+                  <div className=' pl-4 pt-1 pb-3 flex justify-between border-t-1 border-t-gray-400 cols-span-1'>
+                    <span className='w-full truncate'>02-19-2024</span>
+                    <div className='min-w-[7rem] flex gap-x-2 pl-3 items-center border-t-gray-400 '>
+                      <i class=" fa-solid fa-pen-to-square cursor-pointer"></i>
+                      <i class="fa-solid fa-trash cursor-pointer"></i>
+                      <i class="fa-solid fa-bars cursor-pointer"></i>
+
+                  </div>
+
+                  </div>
+                  
+                </div>
+
+                <div className='min-w-[94rem] text-xl h-fit font-semibold grid grid-cols-6 justify-between cursor-pointer hover:bg-gray-300'>
+                  <div className='  px-4 pt-1 pb-3  border-t-1 border-t-gray-400 cols-span-1'>
+                    <span>02-19-2024</span>
+                  </div>
+                  <div className='  px-4 pt-1 pb-3  border-t-1 border-t-gray-400 cols-span-1'>
+                    <span>Olivia Harper</span>
+
+                  </div>
+                  <div className='  px-4 pt-1 pb-3  border-t-1 border-t-gray-400 cols-span-1'>
+                    <span>Perlas ng silanganan</span>
+
+                  </div>
+                  <div className='  px-4 py-4  border-t-1 border-t-gray-400 cols-span-1'>
+                    <span className='text-white bg-[#9C7744] border-1 border-black rounded-md px-15 py-1'>Accepted</span>
+
+                  </div>
+                  <div className='  px-4 py-4  border-t-1 border-t-gray-400 cols-span-1'>
+                    <span className='text-white bg-[#9C7744] border-1 border-black rounded-md px-15 py-1'>Accepted</span>
+
+                  </div>
+                  <div className=' pl-4 pt-1 pb-3 flex justify-between border-t-1 border-t-gray-400 cols-span-1'>
+                    <span className='w-full truncate'>02-19-2024</span>
+                    <div className='min-w-[7rem] flex gap-x-2 pl-3 items-center border-t-gray-400 '>
+                      <i class=" fa-solid fa-pen-to-square cursor-pointer"></i>
+                      <i class="fa-solid fa-trash cursor-pointer"></i>
+                      <i class="fa-solid fa-bars cursor-pointer"></i>
+
+                  </div>
+
+                  </div>
+                  
+                </div>
+
+                <div className='min-w-[94rem] text-xl font-semibold h-fit grid grid-cols-6 justify-between cursor-pointer hover:bg-gray-300'>
+                  <div className='  px-4 pt-1 pb-3  border-t-1 border-t-gray-400 cols-span-1'>
+                    <span>02-19-2024</span>
+                  </div>
+                  <div className='  px-4 pt-1 pb-3  border-t-1 border-t-gray-400 cols-span-1'>
+                    <span>Olivia Harper</span>
+
+                  </div>
+                  <div className='  px-4 pt-1 pb-3  border-t-1 border-t-gray-400 cols-span-1'>
+                    <span>Perlas ng silanganan</span>
+
+                  </div>
+                  <div className='  px-4 py-4  border-t-1 border-t-gray-400 cols-span-1'>
+                    <span className='text-white bg-[#9C7744] border-1 border-black rounded-md px-15 py-1'>Accepted</span>
+
+                  </div>
+                  <div className='  px-4 py-4  border-t-1 border-t-gray-400 cols-span-1'>
+                    <span className='text-white bg-[#9C7744] border-1 border-black rounded-md px-15 py-1'>Accepted</span>
+
+                  </div>
+                  <div className=' pl-4 pt-1 pb-3 flex justify-between border-t-1 border-t-gray-400 cols-span-1'>
+                    <span className='w-full truncate'>02-19-2024</span>
+                    <div className='min-w-[7rem] flex gap-x-2 pl-3 items-center border-t-gray-400 '>
+                      <i class=" fa-solid fa-pen-to-square cursor-pointer"></i>
+                      <i class="fa-solid fa-trash cursor-pointer"></i>
+                      <i class="fa-solid fa-bars cursor-pointer"></i>
+
+                  </div>
+
+                  </div>
+                  
+                </div>
+                 
+                  
+                
+              </div>
+
+            </div>
+            
+          </div>
+
+          
         </div>
       </div>
     </>
